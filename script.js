10a64d66f2f46dc80b589b4b883e6223cabbc915
calebeskes <57757860+calebeskes@users.noreply.github.com>
2019-12-18 08:28:27 -0500

    Added pictures and a quiz

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+var question = "What company is the creater of Pong?";
+var answers = ['Sega', 'Nintendo', 'Atari', 'Microsoft'];
+var answerButtons=document.getElementsByClassName("quizButton");
+var correct = 2; 
